695fdf520e9b63014d23848d03f36600ce80ed24
Bhavesh Patil <bhavesh.patil@perennialsys.com>
2023-01-16 18:59:45 +0530

    Update progress on notification, cancel event, broadcast event action.

--- app/src/main/java/com/perennial/downloadfilenotification/MainActivity.kt
+++ app/src/main/java/com/perennial/downloadfilenotification/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.perennial.downloadfilenotification
 
 import android.Manifest
+import android.app.PendingIntent
 import android.app.ProgressDialog
-import android.content.Intent
+import android.content.*
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.AsyncTask
 import android.os.Build
@@ -13,11 +13,14 @@ import android.os.Bundle
 import android.os.Environment
 import android.provider.Settings
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.perennial.downloadfilenotification.databinding.ActivityMainBinding
@@ -26,6 +29,7 @@ import java.io.FileOutputStream
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -40,12 +44,46 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var bitmap: Bitmap
     private lateinit var absolutePath: String
+    private lateinit var notificationManager: NotificationManagerCompat
+    private lateinit var notification: NotificationCompat.Builder
+    private lateinit var pIntent: PendingIntent
+    private lateinit var pendingIntent: PendingIntent
+
+    private lateinit var taskEventReceiver: TaskEventReceiver
+    private var isTaskCancelled: Boolean = false
+    private var notificationID: Int = Random().nextInt()
+    lateinit var downloadAysncTask: DownloadFileTask
+
+    val channelID: String = "Download Notification"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        mProgressDialog = ProgressDialog(this)
+
+        notificationManager = NotificationManagerCompat.from(this)
+        taskEventReceiver = TaskEventReceiver()
+        downloadAysncTask = DownloadFileTask()
+
+        // adding action for broadcast
+        val activityIntent = Intent(this, TaskEventReceiver()::class.java)
+        activityIntent.putExtra("actionPerformed", "Action Performed")
+        pIntent = PendingIntent.getBroadcast(this, 0, activityIntent, PendingIntent.FLAG_IMMUTABLE)
+
+
         val button = findViewById<Button>(R.id.notificationButton)
+        binding.btnViewPDF.visibility = View.GONE
+        binding.btnViewPDF.setOnClickListener {
+            openPDF()
+        }
+
+        binding.btnViewPDF.text = "Cancel Task"
+
+        binding.btnViewPDF.setOnClickListener {
+            downloadAysncTask.cancel(true)
+            notification.setContentTitle("Downloading cancelled..")
+            notification.setContentText("File downloading cancelled..")
+            notificationManager.notify(notificationID, notification.build())
+        }
 
         if (checkPermission()) {
             toast("Permission already granted: create folder")
@@ -56,7 +94,134 @@ class MainActivity : AppCompatActivity() {
         }
 
         button.setOnClickListener {
-            MyAsyncTask().execute("https://cdn.pixabay.com/photo/2018/11/29/20/01/nature-3846403_960_720.jpg")
+            //https://www.shabakeh-mag.com/sites/default/files/files/attachment/1397/04/1530550032.pdf
+            //https://kotlinlang.org/docs/kotlin-reference.pdf
+            downloadAysncTask.execute("https://www.shabakeh-mag.com/sites/default/files/files/attachment/1397/04/1530550032.pdf")
+            binding.btnViewPDF.visibility = View.VISIBLE
+
+        }
+    }
+
+    inner class DownloadFileTask() : AsyncTask<String?, Int?, String?>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+            notification = NotificationCompat.Builder(this@MainActivity, channelID)
+                .setSmallIcon(R.drawable.ic_baseline_file_download_24)
+                .setContentTitle("File Downloading...")
+                .setContentText("Downloading in progress...")
+                .setPriority(NotificationCompat.PRIORITY_LOW)
+                .setOngoing(false)
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true)
+//                .setContentIntent(pendingIntent)
+                .addAction(R.drawable.ic_launcher_background, "CANCEL", pIntent)
+            notificationManager.notify(notificationID, notification.build())
+
+            /*  mProgressDialog.setMessage("Downloading file...")
+              mProgressDialog.setIcon(R.drawable.ic_launcher_background)
+              mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
+              mProgressDialog.setCancelable(true)
+              mProgressDialog.setCanceledOnTouchOutside(false)
+              mProgressDialog.show()*/
+        }
+
+        override fun doInBackground(vararg strings: String?): String? {
+            try{
+                val imageUrl = URL(strings[0])
+                val conn = imageUrl.openConnection() as HttpURLConnection
+                conn.doInput = true
+                conn.connect()
+                val inputStream = conn.inputStream
+                val fileLength = conn.contentLength
+
+                val file = File("$absolutePath/kotlinR3.pdf")
+                if (file.exists()) {
+                    file.delete()
+                }
+                Log.d("Download fileLength", "$file")
+
+                val data = ByteArray(1024)
+                var count: Int
+                var total: Long = 0
+
+//                val options = BitmapFactory.Options()
+//                options.inPreferredConfig = Bitmap.Config.RGB_565
+//                bitmap = BitmapFactory.decodeStream(inputStream, null, options)!!
+
+                val output = FileOutputStream(file)
+                while (inputStream.read(data).also { count = it } != -1) {
+                    if (isCancelled) {
+                        return null
+                    }
+                    total += count.toLong()
+                    if (fileLength > 0)
+                        publishProgress((total * 100 / fileLength).toInt())
+                    Log.d("Download progress", "${(total * 100 / fileLength).toInt()}")
+
+                    output.write(data, 0, count)
+                }
+                output.flush()
+                output.close()
+                inputStream.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onProgressUpdate(vararg progress: Int?) {
+            super.onProgressUpdate(*progress)
+
+                binding.notificationButton.text = "Downloading.. ${progress[0]}%"
+                notification.setContentText("${progress[0]}%")
+                notification.setProgress(100, progress[0]!!, false)
+                notificationManager.notify(notificationID, notification.build())
+        }
+
+        override fun onCancelled(result: String?) {
+            super.onCancelled(result)
+            Log.d("onCancelled", result.toString())
+        }
+
+        override fun onPostExecute(result: String?) {
+//            mProgressDialog.dismiss()
+//            binding.ivDownloadPicture.setImageBitmap(result)
+            Log.d("Download error", "$result")
+
+            if (result != null) Toast.makeText(
+                this@MainActivity,
+                "Download error: $result",
+                Toast.LENGTH_LONG
+            ).show() else {
+                Toast.makeText(this@MainActivity, "File downloaded", Toast.LENGTH_SHORT)
+                    .show()
+                binding.notificationButton.text = "Download completed"
+                notification.setContentTitle("File Downloaded")
+                notification.setContentText("Downloading completed.")
+                notificationManager.notify(notificationID, notification.build())
+                binding.btnViewPDF.text = "Open File"
+            }
+
+            if (isTaskCancelled) {
+                notification.setContentTitle("File Downloaded")
+                notification.setContentText("Downloading completed.")
+                notificationManager.notify(notificationID, notification.build())
+                binding.btnViewPDF.visibility = View.VISIBLE
+                binding.btnViewPDF.text = "Download cancelled"
+
+            }
+        }
+    }
+
+    inner class TaskEventReceiver: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val isTrue = intent?.getStringArrayExtra("actionPerformed")
+            if (isTrue != null) {
+                downloadAysncTask.cancel(true)
+            }
+
         }
     }
 
@@ -103,6 +268,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun openPDF() {
+        val pdfFile = File("$absolutePath/kotlinR2.pdf") // -> filename = maven.pdf
+        Log.d("Download fileLength", "$pdfFile")
+
+        val path = Uri.fromFile(pdfFile)
+        val pdfIntent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(path.toString())
+        pdfIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        pdfIntent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+        try {
+            startActivity(pdfIntent)
+        } catch (e: ActivityNotFoundException) {
+            Toast.makeText(
+                this@MainActivity,
+                "No Application available to view PDF",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
 
     private val storageActivityResultLauncher =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
@@ -130,76 +315,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-    inner class MyAsyncTask() : AsyncTask<String?, Int?, Bitmap?>() {
-
-        override fun onPreExecute() {
-            super.onPreExecute()
-            mProgressDialog.setMessage("Downloading file...")
-            mProgressDialog.setIcon(R.drawable.ic_launcher_background)
-            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
-            mProgressDialog.show()
-        }
-
-        override fun doInBackground(vararg strings: String?): Bitmap? {
-            try {
-                val imageUrl = URL(strings[0])
-                val conn = imageUrl.openConnection() as HttpURLConnection
-                conn.doInput = true
-                conn.connect()
-                val inputStream = conn.inputStream
-                val fileLength = conn.contentLength
-                val file = File("$absolutePath/image.jpg")
-                if (file.exists()) {
-                    file.delete()
-                }
-                Log.d("Download fileLength", "$file")
-
-                val data = ByteArray(1024)
-                var count: Int
-                var total: Long = 0
-                val options = BitmapFactory.Options()
-                options.inPreferredConfig = Bitmap.Config.RGB_565
-                bitmap = BitmapFactory.decodeStream(inputStream, null, options)!!
-
-                val output = FileOutputStream(file)
-                while (inputStream.read(data).also { count = it } != -1) {
-                    if (isCancelled) return null
-                    total += count.toLong()
-                    if (fileLength > 0)
-                        publishProgress((total * 100 / fileLength).toInt())
-                    Log.d("Download progress", "${(total * 100 / fileLength).toInt()}")
-
-                    output.write(data, 0, count)
-                }
-                output.flush()
-                output.close()
-                inputStream.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-            return bitmap
-        }
-
-        override fun onProgressUpdate(vararg progress: Int?) {
-            super.onProgressUpdate(*progress)
-            // if we get here, length is known, now set indeterminate to false
-            mProgressDialog.setIndeterminate(false)
-            mProgressDialog.setMax(100)
-            mProgressDialog.setProgress(progress[0]!!)
-        }
-
-        override fun onPostExecute(result: Bitmap?) {
-            mProgressDialog.dismiss()
-            binding.ivDownloadPicture.setImageBitmap(result)
-            Log.d("Download error", "$result")
-            if (result != null) Toast.makeText(
-                this@MainActivity,
-                "Download error: $result",
-                Toast.LENGTH_LONG
-            ).show() else Toast.makeText(this@MainActivity, "File downloaded", Toast.LENGTH_SHORT)
-                .show()
-        }
-    }
 
     private fun createFolder() {
         //folder name
@@ -209,7 +324,7 @@ class MainActivity : AppCompatActivity() {
             File("${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)}/$folderName")
         if (file.exists()) {
             absolutePath = file.absolutePath
-        }else{
+        } else {
             val folderCreated = file.mkdir()
             if (folderCreated) {
                 absolutePath = file.absolutePath
@@ -220,7 +335,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
 
     fun toast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
